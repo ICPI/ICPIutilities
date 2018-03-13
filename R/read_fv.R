457a38998f0e289c663f7c9d604ff85d9fad7dab
achafetz <achafetz@usaid.gov>
2018-03-13 17:20:41 -0400

    add option around lowercase

--- R/read_fv.R
+++ R/read_fv.R
@@ -5,6 +5,7 @@
 #'
 #' @param file What is the file name, eg ICPI_FactView_PSNU_20171222_v2_1.txt?
 #' @param path file path to the parent folder, default = "~/ICPI/Data/"
+#' @param to_lower do you want to convert all names to lower case, default = TRUE
 #' @param save_rds save the Fact View as an Rds file, default = TRUE
 #' @param remove_txt should the txt file be removed, default = FALSE
 #'
@@ -16,17 +17,19 @@
 #'#import Q1 PSNU
 #'  df_pnsu <-  read_fv("ICPI_FactView_PSNU_20180215_v1_3.txt", path = "~/Downloads/", save_rds = FALSE)
 #'
-read_fv <- function(file, path = "~/ICPI/Data/", save_rds = TRUE, remove_txt = FALSE){
+read_fv <- function(file, path = "~/ICPI/Data/", to_lower = TRUE, save_rds = TRUE, remove_txt = FALSE){
   #ensure file ends in .txt
    if(stringr::str_detect(file, ".txt") == FALSE) file <- paste0(file, ".txt")
 
   #import
     df <- readr::read_tsv(file.path(path, file),
                           guess_max = 500000)
-  #rename to lower for ease of use
-    df <- dplyr::rename_all(df, tolower)
+
   #change mechid to character for ease of use
-    if("mechanismid" %in% names(df)) df <- dplyr::mutate(df, mechanismid = as.character(mechanismid))
+    if("MechanismID" %in% names(df)) df <- dplyr::mutate(df, MechanismID = as.character(MechanismID))
+
+  #rename to lower for ease of use
+    if(to_lower == TRUE) df <- dplyr::rename_all(df, ~ tolower(.))
 
   #save as Rds
     newfile <- stringr::str_replace(file, "txt", "Rds")
