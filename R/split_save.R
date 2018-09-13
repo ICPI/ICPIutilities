6d9a1a71b47da860efd9ae4e0f4ff09e503410aa
Aaron <achafetz@usaid.gov>
2018-09-13 17:03:19 -0400

    NSE -> allow user to specify grouping variable

--- R/split_save.R
+++ R/split_save.R
@@ -1,8 +1,10 @@
 #' Export a single dataset into multiple files by group
 #'
 #' @param df dataframe to split
+#' @param group_var grouping variable to split the dataset by, eg operatingunit, fundingagency
 #' @param folderpath directory where you want to store the files
 #' @param filename_stub generic stub for naming all the files
+#' @param include_date include date after filenamestub? default = FALSE, eg "20180913"
 #'
 #' @export
 #'
@@ -10,16 +12,32 @@
 #'
 #' @examples
 #' \dontrun{
-#'   split_save(df, "~/CountryFiles", "FY18Q3_TX")
+#' #create country specific files for TX_NEW
+#'  df_mer %>%
+#'   filter(indicator == "TX_NEW",
+#'          standardizeddisaggregate == "Total Numerator") %>%
+#'   split_save(operatingunit, "~/CountryFiles", "FY18Q3_TX")
 #' }
 
-split_save <- function(df, folderpath, filename_stub){
-  df %>%
-    split(.$operatingunit) %>%
-    purrr::walk(~.x %>%
-                  readr::write_csv(file.path(folderpath,
-                                             paste0(filename_stub, "_", unique(.$operatingunit), ".csv"),
-                                   na = "")))
+split_save <- function(df, group_var, folderpath, filename_stub, include_date = FALSE){
+
+  #enquote group var due to NSE
+    group_var <- dplyr::enquo(group_var)
+
+  #get a list of distinct memebers of the user defined grouping variable
+    grp_members <- df %>%
+      dplyr::distinct(!!group_var) %>%
+      dplyr::pull()
+
+  #include date in file name if specified
+    if(include_date == TRUE) paste(filename_stub, format(Sys.Date(), "%Y%m%d"), sep ="_")
+
+  #export one file for each group member
+    purrr::walk(.x = grp_members,
+                .f = ~ df %>%
+                  dplyr::filter(!!group_var == .x) %>%
+                  readr::write_csv(file.path(folderpath, paste0(filename_stub, "_", .x, ".csv")),
+                                   na = ""))
 }
 
 
