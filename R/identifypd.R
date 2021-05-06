decdf35ccd93a370e2e1727a01cb02effb660a6f
Aaron Chafetz <achafetz@usaid.gov>
2021-05-06 13:24:03 -0400

    decommission targets as an option for identifypd

--- R/identifypd.R
+++ R/identifypd.R
@@ -1,7 +1,7 @@
 #' Extract lastest period of dataset
 #' @description This function is pulled from the achafetz/PartnerProgress repo
 #' @param df dataset to use to find latest period
-#' @param pd_type what is returned? (a) full, eg fy2018q1; (b)year, eg 2018; (c) quarter, eg 1; (d) target, eg fy2018_targets
+#' @param pd_type what is returned? (a) full, eg fy2018q1; (b)year, eg 2018; (c) quarter, eg 1
 #' @param pd_prior do you want the last period returned (instead of the current); default = FALSE
 #'
 #' @export
@@ -14,8 +14,6 @@
 #' identifypd(df_mer)
 #' #return the current quarter, eg 3
 #' identifypd(df_mer, "quarter")
-#' #return the current target, eg fy2018_targets
-#' identifypd(df_mer, "target")
 #' #return the prior quarter, eg "fy2018q2"
 #' identifypd(df_mer, pd_prior = TRUE)
 #'   }
@@ -68,9 +66,6 @@ identifypd <- function(df, pd_type = "full", pd_prior = FALSE) {
   } else if(pd_type == "quarter") {
     pd <- stringr::str_sub(pd, start = -1) %>%
       as.integer(.)
-  } else if(pd_type == "target") {
-    pd <- stringr::str_replace(pd, "q[:digit:]", "_targets") #if lower case
-    pd <- stringr::str_replace(pd, "Q[:digit:]", "_TARGETS") #if camel case (MSD standard)
   } else {
     pd
   }
